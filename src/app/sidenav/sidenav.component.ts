f8f7e067c1461a5d1ae2137d96c1204d3554f3fc
Catarina <jcatarina.consoli@gmail.com>
2019-06-11 20:02:51 -0300

    update sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'sidenav',
   templateUrl: 'sidenav.component.html',
-  styleUrls: ['sidenav.component.css'],
+  styleUrls: ['sidenav.component.scss'],
 })
 export class SidenavComponent {}
 
